b98f54af5beae7deba3052c81fdc81d5f9c6447d
Manpreet <manpreetrahal27@gmail.com>
2022-12-29 00:04:49 -0700

    added: vue router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
-import AdminSignUp from "./components/Admin/SignUp";
-import AdminLogin from "./components/Admin/AdminLogin";
-import axios from "axios";
+// import axios from "axios";
 
-const logout = async () => {
-  const a = axios.create({ withCredentials: true });
-  const res = await a.post("http://127.0.0.1:5000/admin/logout", {});
-  console.log(res.data);
-};
+// const logout = async () => {
+//   const a = axios.create({ withCredentials: true });
+//   const res = await a.post("http://127.0.0.1:5000/admin/logout", {});
+//   console.log(res.data);
+// };
 </script>
 
 <template>
-  <AdminSignUp />
-  <AdminLogin />
-  <button @click="logout">logout</button>
+  <router-view />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import AdminLogin from "./components/Admin/AdminLogin";
+import AdminSignup from "./components/Admin/SignUp";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/admin/login", component: AdminLogin },
+  { path: "/admin/signup", component: AdminSignup },
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(""),
+  routes, // short for `routes: routes`
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
